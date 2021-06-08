808747c1001b9f489a2d04db0d1eb456b379c041
daarko10 <daarko10@gmail.com>
2021-06-08 16:43:56 +0300

    Added Flask App

---
+++ app.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi_offline import FastAPIOffline
+
+app = FastAPIOffline()
+
+
+@app.get('/')
+def route():
+    return {"message": "test"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0')
