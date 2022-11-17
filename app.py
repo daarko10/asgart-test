24880eb1bdc3819a419d653fab83eb6435de34cc
daark <daarko10@gmail.com>
2022-11-17 16:45:04 +0200

    shitshow

--- app.py
+++ app.py
@@ -9,15 +9,5 @@ def route():
     return {"message": "test"}
 
 
-@app.get('/saadon')
-def route2():
-    return 'Saadon is gay'
-
-
-@app.get('/dago')
-def route2():
-    return 'dago is the worst ever'
-
-
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0')
